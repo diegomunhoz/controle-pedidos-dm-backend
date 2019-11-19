c1b250463d32aa41267b49861cfd68ed250c5e8b
Diego Munhoz <diego.fatec@hotmail.com>
2019-11-19 16:50:26 -0300

    Salvando URL da imagem do cliente

--- src/main/java/com/diegomunhoz/controlepedidosdm/services/ClienteService.java
+++ src/main/java/com/diegomunhoz/controlepedidosdm/services/ClienteService.java
@@ -115,7 +115,17 @@ public class ClienteService {
 	}
 	
 	public URI uploadProfilePicture(MultipartFile multipartFile) {
-		return s3Service.uploadFile(multipartFile);
+		UserSS user = UserService.authenticated();
+		
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		URI uri = s3Service.uploadFile(multipartFile);
+		
+		Cliente cli = repo.findOne(user.getId());
+		cli.setImageUrl(uri.toString());
+		repo.save(cli);
+		return uri;
 	}
 
 }
